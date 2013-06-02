a504386038cee1d52b2a36d30e4442b626d605ab
Rob Passante <rob@dynamicinternetdevelopment.net>
2013-06-02 19:24:47 -0400

    error handling updates

--- 06-03-13/js/app.js
+++ 06-03-13/js/app.js
@@ -15,6 +15,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        alert('device ready');
         angular.element(document).ready(function() {
             angular.bootstrap(document);
         });
